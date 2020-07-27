5374220194005c5b0e27edc043d73789103a569d
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-27 15:45:06 -0700

    user form input to localStorage

--- 1-user-info/user-info.js
+++ 1-user-info/user-info.js
@@ -1,5 +1,17 @@
+import { makeUser, saveToLocalStorage } from '../1-user-info/userUtils.js';
+
 // import functions and grab DOM elements
+const form = document.querySelector('form');
 
 // initialize state
 
 // set event listeners to update state and DOM
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const newUser = makeUser(formData);
+
+    saveToLocalStorage('USER', newUser);
+});
---
+++ 1-user-info/userUtils.js
@@ -0,0 +1,18 @@
+export function makeUser(formData) {
+    return {
+        name: formData.get('user-name'),
+        pack: formData.get('back-pack'),
+        completed: {}
+    };
+}
+
+export function saveToLocalStorage(dataKey, data) {
+    const stringyData = JSON.stringify(data);
+    localStorage.setItem(dataKey, stringyData);
+}
+
+export function getFromLocalStorage(dataKey) {
+    const stringyData = localStorage.getItem(dataKey);
+    const data = JSON.parse(stringyData);
+    return data;
+}
