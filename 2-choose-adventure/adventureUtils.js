8c3e37012aeb515de35fe005b7435a8fd89baf0d
Heather Peterson <hpeterson462@gmail.com>
2020-07-28 10:55:50 -0700

    getting value of radio tags

--- 2-choose-adventure/adventureUtils.js
+++ 2-choose-adventure/adventureUtils.js
@@ -2,29 +2,32 @@
 
 // import { adventureData } from '../Data/data.js';
 
-export function findById(array, id){
+export function findById(array, id) {
     let adventureMatch = [];
-    for (let i = 0; i < array.length; i++){
-        if (array[i].id === id){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id === id) {
             adventureMatch = array[i];
             return adventureMatch;
         }
     }
 }
 
-export function renderAdventure(adventureData){
+export function renderAdventure(adventureData) {
     const section = document.createElement('section');
 
     const label = document.createElement('label');
     const input = document.createElement('input');
     input.type = 'radio';
-    input.value = 'adventure1';
+    input.value = adventureData.id;
     input.name = 'adventure-choice';
     input.classList.add('pack-div');
-    
+
+
+
+
     const image = document.createElement('img');
     image.src = adventureData.image;
-    
+
     label.append(input, image);
 
     section.append(label);
--- 2-choose-adventure/adventures.js
+++ 2-choose-adventure/adventures.js
@@ -5,6 +5,7 @@ import { renderAdventure } from './adventureUtils.js';
 
 // initialize state
 const main = document.querySelector('main');
+
 //let completedAdventures = 0;
 //const param = new URLSearchParams(window.location.search);
 
@@ -17,4 +18,23 @@ for (let i = 0; i < adventureData.length; i++) {
     main.appendChild(element);
 }
 
+const adventureRadioTags = document.querySelectorAll('input:checked');
+console.log(adventureRadioTags);
+
+/*adventureRadioTags.forEach((adventureTag, i) => {
+    if (i === 0) {
+        adventureTag.value = adventureData[i];
+    } else if (i === 1); {
+        adventureTag.value = adventureData[i];
+    }
+    console.log(adventureTag);
+});*/
+
+adventureRadioTags.forEach((adventureTag) => {
+    adventureTag.addEventListener('click', e => {
+        const userChoice = e.target.value;
+        console.log(e.target.value);
+        return userChoice;
+    });
+});
 
