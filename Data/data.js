a3467d80eab417c77c150b72809adde7d0fe32b0
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-28 11:34:08 -0700

    add all letters and imgs to data.js

--- Data/data.js
+++ Data/data.js
@@ -1,138 +1,206 @@
 const adventure1 = {
-
     id: 'adventure1',
-   
     title: 'Adventure 1',
-   
-    map: {
-   
-        top: 'location here%',
-   
-        left: 'location here%'
-   
-    },
-   
-    image: '../assets/blue.png',
-   
+    image: '../assets/adventure1.png',
     description: `
-   
            Review the letters below. When you are ready, click the "Let's Play" button at the bottom of the page to navigate to the game page. Happy studying! :) `,
-   
     letterChoices: [{
-   
         id: 'A',
-   
+        image: '../assets/data-letters/A.png',
         description: 'The uppercase letter A. This is the 1st letter in the English alphabet!',
-   
         result: 'Great job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
-   
         id: 'B',
-   
+        image: '../assets/data-letters/B.png',
         description: 'The uppercase letter B. This is the 2nd letter in the English alphabet!',
-   
         result: 'Nice job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
-   
         id: 'C',
-   
+        image: '../assets/data-letters/C.png',
         description: 'The uppercase letter C. This is the 3rd letter in the English alphabet!',
-   
         result: 'Amazing job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
         id: 'D',
-        
+        image: '../assets/data-letters/D.png',
         description: 'The uppercase letter D. This is the 4th letter in the English alphabet!',
-   
         result: 'Awesome job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
         id: 'E',
-        
+        image: '../assets/data-letters/E.png',
         description: 'The uppercase letter E. This is the 5th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
     }]
-    
 };
-const adventure2 = {
 
+const adventure2 = {
     id: 'adventure2',
-   
     title: 'Adventure 2',
-   
-    map: {
-   
-        top: 'location here%',
-   
-        left: 'location here%'
-   
-    },
-   
-    image: '../assets/green.png',
-   
+    image: '../assets/adventure2.png',
     description: `
-   
            Review the letters below. When you are ready, click the "Let's Play" button at the bottom of the page to navigate to the game page. Happy studying! :) `,
-   
     letterChoices: [{
-   
-        id: 'A',
-   
-        description: 'The uppercase letter A. This is the 1st letter in the English alphabet!',
-   
+        id: 'F',
+        image: '../assets/data-letters/F.png',
+        description: 'The uppercase letter F. This is the 6th letter in the English alphabet!',
         result: 'Great job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
-   
-        id: 'B',
-   
-        description: 'The uppercase letter B. This is the 2nd letter in the English alphabet!',
-   
+        id: 'G',
+        image: '../assets/data-letters/G.png',
+        description: 'The uppercase letter G. This is the 7th letter in the English alphabet!',
         result: 'Nice job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
-   
-        id: 'C',
-   
-        description: 'The uppercase letter C. This is the 3rd letter in the English alphabet!',
-   
+        id: 'H',
+        image: '../assets/data-letters/H.png',
+        description: 'The uppercase letter H. This is the 8th letter in the English alphabet!',
         result: 'Amazing job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
-        id: 'D',
-        
-        description: 'The uppercase letter D. This is the 4th letter in the English alphabet!',
-   
+        id: 'I',
+        image: '../assets/data-letters/I.png',
+        description: 'The uppercase letter I. This is the 9th letter in the English alphabet!',
         result: 'Awesome job! You earned one S(tudy) Coin!',
-   
         sCoin: 1,
-   
     }, {
-        id: 'E',
-        
-        description: 'The uppercase letter E. This is the 5th letter in the English alphabet!',
+        id: 'J',
+        image: '../assets/data-letters/J.png',
+        description: 'The uppercase letter J. This is the 10th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
     }]
-    
+};
+
+const adventure3 = {
+    id: 'adventure3',
+    title: 'Adventure 3',
+    image: '../assets/adventure3.png',
+    description: `
+           Review the letters below. When you are ready, click the "Let's Play" button at the bottom of the page to navigate to the game page. Happy studying! :) `,
+    letterChoices: [{
+        id: 'K',
+        image: '../assets/data-letters/K.png',
+        description: 'The uppercase letter K. This is the 11th letter in the English alphabet!',
+        result: 'Great job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'L',
+        image: '../assets/data-letters/L.png',
+        description: 'The uppercase letter L. This is the 12th letter in the English alphabet!',
+        result: 'Nice job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'M',
+        image: '../assets/data-letters/M.png',
+        description: 'The uppercase letter M. This is the 13th letter in the English alphabet!',
+        result: 'Amazing job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'N',
+        image: '../assets/data-letters/N.png',
+        description: 'The uppercase letter N. This is the 14th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'O',
+        image: '../assets/data-letters/O.png',
+        description: 'The uppercase letter O. This is the 15th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }]
+};
+
+const adventure4 = {
+    id: 'adventure4',
+    title: 'Adventure 4',
+    image: '../assets/adventure4.png',
+    description: `
+           Review the letters below. When you are ready, click the "Let's Play" button at the bottom of the page to navigate to the game page. Happy studying! :) `,
+    letterChoices: [{
+        id: 'P',
+        image: '../assets/data-letters/P.png',
+        description: 'The uppercase letter P. This is the 16th letter in the English alphabet!',
+        result: 'Great job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'Q',
+        image: '../assets/data-letters/Q.png',
+        description: 'The uppercase letter Q. This is the 17th letter in the English alphabet!',
+        result: 'Nice job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'R',
+        image: '../assets/data-letters/R.png',
+        description: 'The uppercase letter R. This is the 18th letter in the English alphabet!',
+        result: 'Amazing job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'S',
+        image: '../assets/data-letters/S.png',
+        description: 'The uppercase letter S. This is the 19th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'T',
+        image: '../assets/data-letters/T.png',
+        description: 'The uppercase letter T. This is the 20th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }]
+};
+
+const adventure5 = {
+    id: 'adventure5',
+    title: 'Adventure 5',
+    image: '../assets/adventure5.png',
+    description: `
+           Review the letters below. When you are ready, click the "Let's Play" button at the bottom of the page to navigate to the game page. Happy studying! :) `,
+    letterChoices: [{
+        id: 'U',
+        image: '../assets/data-letters/U.png',
+        description: 'The uppercase letter U. This is the 21st letter in the English alphabet!',
+        result: 'Great job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'V',
+        image: '../assets/data-letters/V.png',
+        description: 'The uppercase letter V. This is the 22nd letter in the English alphabet!',
+        result: 'Nice job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'W',
+        image: '../assets/data-letters/W.png',
+        description: 'The uppercase letter W. This is the 23rd letter in the English alphabet!',
+        result: 'Amazing job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'X',
+        image: '../assets/data-letters/X.png',
+        description: 'The uppercase letter X. This is the 24th letter in the English alphabet!',
+        result: 'Awesome job! You earned one S(tudy) Coin!',
+        sCoin: 1,
+    }, {
+        id: 'Y',
+        image: '../assets/data-letters/Y.png',
+        description: 'The uppercase letter Y. This is the 25th letter in the English alphabet!',
+    }, {
+        id: 'Z',
+        image: '../assets/data-letters/Z.png',
+        description: 'The uppercase letter Z. This is the 26th letter in the English alphabet!',
+    }]
 };
 
 const adventureData = [
-    adventure1, adventure2
+    adventure1, 
+    adventure2, 
+    adventure3, 
+    adventure4, 
+    adventure5
 ];
 
 export default adventureData;
