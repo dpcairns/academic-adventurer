fb9769d284dccb86b005d91e89b842967311c70e
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-29 14:34:07 -0700

    half of game is working

--- 3-game-page/game-page.js
+++ 3-game-page/game-page.js
@@ -1,6 +1,6 @@
 // import functions and grab DOM elements
 import adventuresData from '../Data/data.js';
-import { renderGame, findById } from './game-page-utils.js';
+import { renderGame, findById, getRandomLetter } from './game-page-utils.js';
 // initialize state
 const main = document.querySelector('main');
 const params = new URLSearchParams(window.location.search);
@@ -8,6 +8,7 @@ const adventureId = params.get('id');
 const adventureData = findById(adventuresData, adventureId);
 const sectionElement = renderGame(adventureData);
 const backButton = document.querySelector('#back-button');
+const radioTags = sectionElement.querySelectorAll('input[name="letter-select"]');
 
 
 backButton.addEventListener('click', () => {
@@ -17,9 +18,22 @@ backButton.addEventListener('click', () => {
 main.append(sectionElement);
 
 // set event listeners to update state and DOM
-// const letter = getRandomLetter(adventureData.letterChoices);
-// const correctLetter = letter.id;
-// const correctDescription = letter.description;
+const letter = getRandomLetter(adventureData.letterChoices);
+const correctLetter = letter.id;
+//const correctDescription = letter.description;
+console.log(correctLetter);
 
+radioTags.forEach((radioTag) => {
+    radioTag.addEventListener('click', (e) => {
+        const userChoice = e.target.value;
+        if (userChoice === correctLetter) {
+            alert('you won');
+            window.location = '../2-choose-adventure/index.html';
+        } else {
+            alert('try again loser');
+        }
+        console.log(userChoice);
+    });
+    
 
-
+});
